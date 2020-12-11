9b034fff845fd15d332ea9aaff127b3bf2eb6516
Reyhane Ghamsari <ghamsari.reyhane@gmail.com>
2020-12-11 09:53:14 +0330

    Weather json Model commit

---
+++ app/src/main/java/com/ghamsari/weather/Model/DatabaseWeather.java
@@ -0,0 +1,67 @@
+package com.ghamsari.weather.Model;
+
+public class DatabaseWeather {
+    private double Lon;
+    private double Lat;
+    private String Main;
+    private double Speed;
+    private double Temp;
+    private int Humidity;
+
+    public DatabaseWeather() {
+
+    }
+
+    public DatabaseWeather(double lon, double lat) {
+        this.Lon = lon;
+        this.Lat = lat;
+    }
+
+    public double getLon() {
+        return Lon;
+    }
+
+    public double getLat() {
+        return Lat;
+    }
+
+    public String getMain() {
+        return Main;
+    }
+
+    public String getSpeed() {
+        return Speed +" km/hr";
+    }
+
+    public double getTemp() {
+        return Temp ;
+    }
+
+    public String getHumidity() {
+        return Humidity + " %";
+    }
+
+    public void setLon(double lon) {
+        this.Lon = lon;
+    }
+
+    public void setLat(double lat) {
+        this.Lat = lat;
+    }
+
+    public void setMain(String main) {
+        this.Main = main;
+    }
+
+    public void setSpeed(double speed) {
+        this.Speed = speed;
+    }
+
+    public void setTemp(double temp) {
+        this.Temp = temp;
+    }
+
+    public void setHumidity(int humidity) {
+        this.Humidity = humidity;
+    }
+}
