52c08e31b1c6a50f5002945d18e733d9fcfbe79b
Reyhane Ghamsari <ghamsari.reyhane@gmail.com>
2020-12-13 11:53:55 +0330

    Repository commit

--- app/src/main/java/com/ghamsari/weather/network/JsonPlaceHolderApi.java
+++ app/src/main/java/com/ghamsari/weather/network/JsonPlaceHolderApi.java
@@ -4,14 +4,14 @@ import com.ghamsari.weather.model.Weather;
 import com.ghamsari.weather.model.WeatherDto;
 
 import java.util.List;
-import java.util.Observable;
 
+import io.reactivex.Observable;
 import io.reactivex.Single;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface JsonPlaceHolderApi {
     @GET("weather")
-    Single <List<WeatherDto>> getWeather(@Query("lat") String lat ,@Query("lon") String lon ,@Query("units") String units,
-                                         @Query("appid") String key);
+    Observable<List<WeatherDto>> getWeather(@Query("lat") String lat , @Query("lon") String lon , @Query("units") String units,
+                                            @Query("appid") String key);
 }
---
+++ app/src/main/java/com/ghamsari/weather/repository/WeatherRepository.java
@@ -0,0 +1,50 @@
+package com.ghamsari.weather.repository;
+
+import android.app.Application;
+import android.widget.Toast;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.ghamsari.weather.model.WeatherDto;
+import com.ghamsari.weather.network.ApiClient;
+import com.ghamsari.weather.network.JsonPlaceHolderApi;
+
+import java.util.List;
+
+import io.reactivex.Scheduler;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class WeatherRepository {
+    private JsonPlaceHolderApi jsonPlaceHolderApi ;
+    private   MutableLiveData <List<WeatherDto>> mutableLiveData ;
+
+    public WeatherRepository() {
+       jsonPlaceHolderApi = ApiClient.getRetrofitInstance().create(JsonPlaceHolderApi.class);
+    }
+    public LiveData<List<WeatherDto>> getLocationWeatherDitel (String lot ,String lon) {
+        jsonPlaceHolderApi.getWeather(lot, lon, "metric", "6ff87ae2cecff28bf3919d0106f692fb").subscribeOn(Schedulers.io()).
+                observeOn(AndroidSchedulers.mainThread()).subscribe(new DisposableObserver<List<WeatherDto>>() {
+            @Override
+            public void onNext(@NonNull List<WeatherDto> weatherDtos) {
+                mutableLiveData.setValue(weatherDtos);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+        return mutableLiveData;
+}
+}
