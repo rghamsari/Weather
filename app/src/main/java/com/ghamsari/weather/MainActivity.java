4093ef22ac6ad95e0abc7ca1e63f29efda9018f7
Reyhane Ghamsari <ghamsari.reyhane@gmail.com>
2020-12-13 19:19:14 +0330

    getLocation commit

--- app/src/main/java/com/ghamsari/weather/MainActivity.java
+++ app/src/main/java/com/ghamsari/weather/MainActivity.java
@@ -2,7 +2,10 @@ package com.ghamsari.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+
+import com.ghamsari.weather.view.WeatherView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        WeatherView view = new WeatherView();
+        view.getLocation(MainActivity.this);
     }
 }
---
+++ app/src/main/java/com/ghamsari/weather/view/WeatherView.java
@@ -0,0 +1,93 @@
+package com.ghamsari.weather.view;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class WeatherView {
+    private  Context context;
+    private Activity activity;
+
+    private static final int REQUEST_LOCATION = 1;
+    LocationManager locationManager;
+    String latitude, longitude;
+    public void getLocation(Activity activity){
+        this.activity =activity;
+        context =  activity.getApplicationContext();
+        ActivityCompat.requestPermissions( activity, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        locationManager = (LocationManager) activity.getSystemService(activity.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            OnGPS();
+        } else {
+            location();
+        }
+
+
+
+    }
+
+    private void location() {
+        if (ActivityCompat.checkSelfPermission(
+                context,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (locationGPS != null) {
+                double lat = locationGPS.getLatitude();
+                double longi = locationGPS.getLongitude();
+                latitude = String.valueOf(lat);
+                longitude = String.valueOf(longi);
+              //  Toast.makeText(context,"Your Location: " + "\n" + "Latitude: " + latitude + "\n" + "Longitude: " + longitude , Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "Unable to find location.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void OnGPS() {
+        ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+
+//        final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", new  DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//               context.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+//            }
+//        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                dialog.cancel();
+//            }
+//        });
+//        final AlertDialog alertDialog = builder.create();
+//        alertDialog.show();
+    }
+    
+
+
+}
+
+
